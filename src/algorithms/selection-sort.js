963987919f169efaa1f4f9c95adbc5f5f8ef33ff
Samuel Degueldre <sad@odoo.com>
2021-01-30 15:06:31 +0100

    [REF] document selection-sort.js, move yield after swap for consistency.

--- src/algorithms/selection-sort.js
+++ src/algorithms/selection-sort.js
@@ -1,12 +1,17 @@
 import {swap} from '../utils.js';
 
+/**
+ * @param {number[]} arr the array to sort
+ * @returns {Generator} A generator that will progressively sort the underlying
+ *  array
+ */
 export default function* selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let min = i;
         for (let j = i; j < arr.length; j++) {
             min = arr[j] < arr[min] ? j : min;
-            yield;
         }
         swap(arr, i, min);
+        yield;
     }
 }

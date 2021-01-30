83ec22ea08567838cb7b866483c703edd8f57033
Samuel Degueldre <sad@odoo.com>
2021-01-30 13:15:16 +0100

    [REF] document heap-sort.js, use generator delegation, remove dead code

--- src/algorithms/heap-sort.js
+++ src/algorithms/heap-sort.js
@@ -1,43 +1,34 @@
 import {swap} from '../utils.js';
 
+/**
+ * @param {number[]} arr the array to sort
+ * @returns {Generator} A generator that will progressively sort the underlying
+ *  array
+ */
 export default function* heapSort(arr) {
-    const builder = buildMaxHeap(arr);
-    while (!builder.next().done) {
-        yield;
-    }
+    yield* buildMaxHeap(arr);
     for (let i = arr.length - 1; i >= 0; i--) {
         swap(arr, i, 0);
-        const maxifier = maxHeapify(arr, i, 0);
-        while (!maxifier.next().done) {
-            yield;
-        }
+        yield* maxHeapify(arr, i, 0);
     }
 }
-
-function checkHeap(heap, index) {
-    const right = 2 * (index + 1);
-    const left = right - 1;
-    let isHeap = true;
-    if (right < heap.length) {
-        isHeap = (heap[right] <= heap[index] && checkHeap(heap, right));
-    }
-    if (left < heap.length && isHeap) {
-        // https://github.com/eslint/eslint/issues/13780 Should not be needed here but at the declaration.
-        // eslint-disable-next-line no-unused-vars
-        isHeap = (heap[left] <= heap[index] && checkHeap(heap, left));
-    }
-    return isHeap;
-}
-
+/**
+ * @param {number[]} arr the array in which to build the heap
+ * @returns {Generator} A generator that will progressively build a heap in the
+ *  underlying array
+ */
 function* buildMaxHeap(arr) {
     for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
-        const maxifier = maxHeapify(arr, arr.length, i);
-        while (!maxifier.next().done) {
-            yield;
-        }
+        yield* maxHeapify(arr, arr.length, i);
     }
 }
-
+/**
+ * @param {number[]} arr the array to maxHeapify
+ * @param {number} heapSize the index of the end of the heap in the array
+ * @param {number} index the position from which to maxHeapify
+ * @returns {Generator} A generator that will progressively "max-heapify" the
+ *  underlying array
+ */
 function* maxHeapify(arr, heapSize, index) {
     const right = 2 * (index + 1);
     const left = right - 1;
@@ -50,10 +41,7 @@ function* maxHeapify(arr, heapSize, index) {
     }
     if (largest !== index) {
         swap(arr, index, largest);
-        const maxifier = maxHeapify(arr, heapSize, largest);
-        while (!maxifier.next().done) {
-            yield;
-        }
+        yield;
+        yield* maxHeapify(arr, heapSize, largest);
     }
-    yield;
 }
